18e6b2bca14aebb7bedc0701e16f9633683d5b3e
Swizec Teller <swizec@swizec.com>
2013-02-23 19:04:27 +0100

    sort of visualised

--- js/script.js
+++ js/script.js
@@ -16,8 +16,6 @@ var timeline = d3.time.scale()
         .ticks(d3.time.years, 5)
         .scale(timeline);
 
-var x = d3.scale.linear().domain([0, 100]).range([0, width]);
-
 d3.json('stranke.json', function (stranke) {
     var dates = d3.keys(stranke.stranke_all).map(function (id) {
         var stranka = stranke.stranke_all[id];
@@ -27,7 +25,7 @@ d3.json('stranke.json', function (stranke) {
 
     //console.log(dates);
 
-    timeline.domain([d3.min(dates), new Date()]);
+    timeline.domain([new Date(1988), new Date()]);
     y.domain(d3.keys(stranke.stranke_all));
 
     svg.append("g")
@@ -39,6 +37,10 @@ d3.json('stranke.json', function (stranke) {
         var stranka = stranke.stranke_all[id],
             from = new Date(stranka.od[0], stranka.od[1], stranka.od[2]),
             to = new Date(stranka['do'][0], stranka['do'][1], stranka['do'][2]);
+
+        if (stranka.od[0] < 1988) {
+            return;
+        }
         
         svg.append("rect")
             .attr({x: timeline(from),
@@ -47,10 +49,23 @@ d3.json('stranke.json', function (stranke) {
                    height: y.rangeBand()});
             //.style('fill', stranka.barva);
 
-        var postane = stranka.nastala_iz.map(function (id) {
-            var s1 = stranke.stranke_all[id];
+        svg.append('text')
+            .classed('okrajsava', true)
+            .text(stranka.okrajsava)
+            .attr({x: timeline(from)+2,
+                   y: (y(id)+y.rangeBand())-1});
 
-            return {date: new Date(s1.do[0], s1.do[1], s1.do[2]),
+        var postane = stranka.nastala_iz.map(function (id) {
+            var s1 = stranke.stranke_all[id],
+                origin;
+
+            if (s1.do[0] > (new Date()).getFullYear()) {
+                origin = from;
+            }else{
+                origin = new Date(s1.do[0], s1.do[1], s1.do[2]);
+            }
+            
+            return {date: origin,
                     id: s1.id};
         }),
             me = {date: from,
@@ -59,9 +74,9 @@ d3.json('stranke.json', function (stranke) {
         var link = d3.svg.line()
                 .interpolate('basis')
                 .x(function (d) { return timeline(d.date); })
-                .y(function (d) { return y(d.id); });
-                //.interpolate('cardinal')
-                //.tension(0.3);
+                .y(function (d) { return y(d.id)+y.rangeBand()/2; })
+                .interpolate('cardinal')
+                .tension(0.3);
 
         //svg.append('path')
         //    .attr("d", d3.svg.area(postane)
@@ -75,7 +90,7 @@ d3.json('stranke.json', function (stranke) {
                 .datum([me, s1])
                 .attr("d", link)
                 .attr({stroke: 'steelblue',
-                       'stroke-width': y.rangeBand(),
+                       'stroke-width': 2,
                        fill: 'none',
                        opacity: 0.5});
                                         
