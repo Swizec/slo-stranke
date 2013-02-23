b1a2e481bf37119a2dc1be7e9d9dfa56858052fc
Swizec Teller <swizec@swizec.com>
2013-02-23 17:11:09 +0100

    parties on timeline

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 var width = 1024,
-    height = 500,
+    height = 1024,
     padding = 50,
     svg = d3.select('#graph')
         .append('svg')
@@ -9,6 +9,8 @@ var width = 1024,
 
 var timeline = d3.time.scale()
         .range([padding, width-padding]),
+    y = d3.scale.ordinal()
+        .rangeRoundBands([0, height], 0.5, 2),
     axis = d3.svg.axis()
         .orient('top')
         .ticks(d3.time.years, 5)
@@ -21,12 +23,30 @@ d3.json('stranke.json', function (stranke) {
         var stranka = stranke.stranke_all[id];
         return new Date(stranka.od[0], stranka.od[1], stranka.od[2]);
     });
+    dates = dates.sort(d3.ascending);
+
+    //console.log(dates);
 
     timeline.domain([d3.min(dates), new Date()]);
+    y.domain(d3.keys(stranke.stranke_all));
+
+    console.log(y("5"));
 
     svg.append("g")
         .attr("transform", "translate(0,30)")
         .classed('axis', true)
         .call(axis);
 
+    d3.keys(stranke.stranke_all).map(function (id) {
+        var stranka = stranke.stranke_all[id],
+            from = new Date(stranka.od[0], stranka.od[1], stranka.od[2]),
+            to = new Date(stranka['do'][0], stranka['do'][1], stranka['do'][2]);
+        
+        svg.append("rect")
+            .attr({x: timeline(from),
+                   y: y(id),
+                   width: timeline(to)-timeline(from),
+                   height: y.rangeBand()})
+            .style('fill', stranka.barva);
+    });
 });
